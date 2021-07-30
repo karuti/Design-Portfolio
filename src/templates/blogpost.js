e4d724feb2f72746bb39e5b5e0ec9e7dbd765401
FieldPro Design <design@optimetriks.com>
2021-07-30 18:13:39 +0300

    Aded contentful

---
+++ src/templates/blogpost.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+class BlogPostTemplate extends React.Component {
+  render() {
+    return <></>
+  }
+}
+export default BlogPostTemplate
---
+++ src/templates/workpost.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+class WorkPostTemplate extends React.Component {
+  render() {
+    return <></>
+  }
+}
+export default WorkPostTemplate
